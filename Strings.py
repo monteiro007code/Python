b721b95db9ebf7acda1ca453c27f46aeed51857a
Rodrigo <rm8545735@gmail.com>
2025-06-17 18:58:13 -0300

    Strings

---
+++ Strings.py
@@ -0,0 +1,22 @@
+
+# Step 7
+text = 'Hello World'
+print(len(text))
+
+# Step 5
+text = 'Hello World'
+print(text[6])
+
+# Step 5
+text = 'Hello World'
+print(text[-1])
+
+#Step 8
+text = 'Hello World'
+print(type(text))
+
+#Step 15
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet.find(text[0])

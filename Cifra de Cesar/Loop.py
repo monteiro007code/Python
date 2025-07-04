aa9e8fd8533498b61fcc75897df95cb9f2505c80
Rodrigo <rm8545735@gmail.com>
2025-07-04 09:25:34 -0300

    Novas posições

--- Cifra de Cesar/Loop.py
+++ Cifra de Cesar/Loop.py
@@ -3,7 +3,8 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for char in text.lower():
     index = alphabet.find(char)
-    print(char, index)
+   # print(char, index)
     new_index = index + shift
-
+    new_char = alphabet[new_index]
+    print('char:', char, 'new char:', new_char)
     
